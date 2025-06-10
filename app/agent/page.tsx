c05e720c673236cfe7ae124102c22fab738953c6
Jack Jin <jackzhaojin@gmail.com>
2025-06-09 22:53:27 -0400

    TDD as well, refactor: simplify AgentPage layout and enhance AgentFlow UI with improved structure and styling, big upgrade to UI

--- app/agent/page.tsx
+++ app/agent/page.tsx
@@ -4,9 +4,7 @@ import { AgentFlowProvider } from '@/providers/AgentFlowProvider';
 export default function AgentPage() {
   return (
     <AgentFlowProvider>
-      <div className="container mx-auto p-6">
-        <AgentFlow />
-      </div>
+      <AgentFlow />
     </AgentFlowProvider>
   );
 }
